a70a3903c4a84525cfe3cdc0ca79455636e19220
omikronik <yasirceltik9@hotmail.com>
2022-11-05 00:56:01 +0000

    added backup script

---
+++ dot_backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+# Check if git is installed
+GIT_INSTALLED="$(git --version)"
+if [[ $GIT_INSTALLED == *"version"* ]]; then
+    echo "Git is available"
+else
+    echo "Git is not available"
+    exit 1
+fi
+
+# Copy dotfiles
+cp $HOME/{.zshrc,.zshenv,.vimrc,.tmux.conf}
+cp $HOME/.config/{alacritty/alacritty.yml,nvim/init.vim}
+
+
+# Copy vs code files
+cp $HOME/Library/Application\ 
+Support/Code/User/{keybindings.json,settings.json} .
+
+# Copy list of extensions
+code --list-extensions --show-versions >> ../../vscode/extensions.txt
+
+
+# Ceck git status
+GIT_STATUS="$(git status | grep -i "modified")"
+
+# If there is a new change
+if [[ $GIT_STATUS == *"modified"* ]]; then
+    echo "push"
+fi
+
+git add -u;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git puch origin main
