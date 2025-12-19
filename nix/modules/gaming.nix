5cd061ea20c77d21014fbc2cc04e91fa03b41598
omikronik <yasirceltik9@gmail.com>
2025-12-19 20:00:21 +0000

    wine stuff

--- nix/modules/gaming.nix
+++ nix/modules/gaming.nix
@@ -4,11 +4,16 @@
 	environment.variables = {
 		WINEDEBUG = "-all";
 		WINEPREFIX = "$HOME/.wine";
+
+		QT_QPA_PLATFORMTHEME = "kde";
 	};
 
 	environment.systemPackages = with pkgs; [
 		winetricks
 		wineWowPackages.waylandFull
 		lutris
+
+		dxvk
+		vkd3d-proton
 	];
 }
