2ab7bf5db73c97515424e8efc801b0821264d65e
omikronik <yasirceltik9@gmail.com>
2026-05-02 21:28:38 +0100

    temp lutris fix

--- nix/modules/gaming.nix
+++ nix/modules/gaming.nix
@@ -1,19 +1,27 @@
 { config, pkgs, ... }:
-
+let
+  lutris-fixed = pkgs.lutris.override {
+    buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+      multiPkgs = envPkgs:
+        let
+          originalPkgs = args.multiPkgs envPkgs;
+          customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
+        in
+        builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+    });
+  };
+in
 {
-	environment.variables = {
-		WINEDEBUG = "-all";
-		WINEPREFIX = "$HOME/.wine";
-
-		QT_QPA_PLATFORMTHEME = "kde";
-	};
-
-	environment.systemPackages = with pkgs; [
-		winetricks
-		wineWow64Packages.waylandFull
-		lutris
-
-		dxvk
-		vkd3d-proton
-	];
+  environment.variables = {
+    WINEDEBUG = "-all";
+    WINEPREFIX = "$HOME/.wine";
+    QT_QPA_PLATFORMTHEME = "kde";
+  };
+  environment.systemPackages = with pkgs; [
+    winetricks
+    wineWow64Packages.waylandFull
+    lutris-fixed
+    dxvk
+    vkd3d-proton
+  ];
 }
